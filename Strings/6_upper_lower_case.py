945ec7762d169e03d370124fff4aca4b6de82c81
unknown <keshav.wanjale@gmail.com>
2024-09-12 00:11:02 +0530

    [Keshav] Add: Python script for returning the string in upper and lower case.

---
+++ Strings/6_upper_lower_case.py
@@ -0,0 +1,9 @@
+def upper_lower(str):
+    print(f"The given string in uppercase is {str.upper()}",
+      f"\nThe given string in lowercase is {str.lower()}")
+
+
+
+str_input = input("Type any character string to convert it into upper or lower case:")
+upper_lower(str_input)
+
