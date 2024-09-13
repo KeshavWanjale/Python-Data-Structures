cb016cbc90afdf3dbf7fb6f2a066a915907e2f4a
unknown <keshav.wanjale@gmail.com>
2024-09-13 14:16:10 +0530

    [Keshav] Add: Python script for getting the symmetric diffrence of two sets.

---
+++ Set-Programs/9_set_symmetric_difference.py
@@ -0,0 +1,8 @@
+def symmetric_difference_of_sets(my_set1, my_set2):
+    return my_set1.symmetric_difference(my_set2)
+
+
+my_set1 = {1,2,3,4,5}
+my_set2 = {4,5,6,7,8}
+
+print("Symmetric difference:", symmetric_difference_of_sets(my_set1, my_set2))
