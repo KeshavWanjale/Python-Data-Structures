5082907434b6854b10b84b1a6b981318a0b7ca4c
unknown <keshav.wanjale@gmail.com>
2024-09-13 14:15:38 +0530

    [Keshav] Add: Python script for getting the minus(diffrence) of two sets.

---
+++ Set-Programs/8_set_difference.py
@@ -0,0 +1,8 @@
+def difference_of_sets(my_set1, my_set2):
+    return my_set1.difference(my_set2)
+
+
+my_set1 = {1,2,3,4,5}
+my_set2 = {4,5,6,7,8}
+
+print("Symmetric difference:", difference_of_sets(my_set1, my_set2))
