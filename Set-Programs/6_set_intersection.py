793fb046c39c935a1f8aaa919e8a34ee5a418121
unknown <keshav.wanjale@gmail.com>
2024-09-13 14:14:22 +0530

    [Keshav] Add: Python script for getting the intersection of two sets.

---
+++ Set-Programs/6_set_intersection.py
@@ -0,0 +1,8 @@
+def intersection_of_sets(my_set1, my_set2):
+    return my_set1.intersection(my_set2)
+
+
+my_set1 = {1,2,3,4,5}
+my_set2 = {4,5,6,7,8}       
+
+print("Intersection:", intersection_of_sets(my_set1, my_set2))
